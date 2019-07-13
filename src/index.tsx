7a569fd5fd326015e0475471c05cf7036918ae83
Oliver van Komen <olvan18@student.sdu.dk>
2019-07-13 20:45:09 +0200

    ProviderWrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker';
 
-import store from "./store/store";
 import App from './components/App';
 import './index.scss';
+import ProviderWrapper from './store/ProviderWrapper';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<ProviderWrapper><App /></ProviderWrapper>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
