24b1f4ce27b3c0e0602aef7826005f5d21b8cde2
Robin Louis van Komen <rlvk@typoconsult.dk>
2019-07-15 15:16:52 +0200

    add prettier

--- src/store/middlewares.ts
+++ src/store/middlewares.ts
@@ -4,8 +4,8 @@ import { Middleware } from "redux";
 
 const middlewares: Array<Middleware> = [thunk];
 
-if (process.env.NODE_ENV === 'development') {
-    middlewares.push(logger);
+if (process.env.NODE_ENV === "development") {
+	middlewares.push(logger);
 }
 
 export default middlewares;
